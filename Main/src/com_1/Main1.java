763399fffa4b73a9445ecfe9cc45663566c92a52
labdemo80 <labdemo80@gmail.com>
2026-02-11 19:19:47 +0530

    Changes made in Main1

--- Main/src/com_1/Main1.java
+++ Main/src/com_1/Main1.java
@@ -1,7 +1,11 @@
 package com_1;
 
 public class Main1 {
+	public void add(int a,int b) {
+		System.out.println(a+b);
+	}
 	public static void main(String[] args) {
-		System.out.println("Heloo User!!!");
+		Main1 main1 = new Main1();
+		main1.add(4, 2);
 	}
 }
